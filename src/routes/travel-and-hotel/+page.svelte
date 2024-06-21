7cf49a932db13ebf20ffc951dd18c729611a026b
Ash Johns <mail@ashpjohns.com>
2024-06-21 14:31:15 -0400

    add travel and hotel info

---
+++ src/routes/travel-and-hotel/+page.svelte
@@ -0,0 +1,35 @@
+<h1>Travel &amp; Hotel</h1>
+<p>
+	Our wedding, both ceremony and reception, will be held at Butternut Farm Golf Club in Stow,
+	Massachusetts. About 25 minutes northeast of Worcester and 45 minutes west of Boston, Stow is a
+	small town known for apple picking and beautiful fall foliage.
+</p>
+
+<h2>Hotel</h2>
+<p>
+	We've organized a hotel block at the Embassy Suites by Hilton Boston Marlborough. Below is the
+	booking information so you can get a room at a discounted rate, because everyone loves a bargain.
+</p>
+<p>
+	Call the hotel at 508-485-5900 and select the option “0” to speak with a front desk agent and ask
+	to book a room under the LeKania Wedding Or Follow the below link to our personal <a
+		href="https://www.hilton.com/en/attend-my-event/bosmaes-906-a1b8c773-d681-41f4-93ce-4bac4637ae23/"
+		>booking website</a
+	>!
+</p>
+<p>
+	Please don't hesitate to text or email us with any questions or if you run into any issues while
+	booking!
+</p>
+
+<h2>Transportation</h2>
+<p>
+	For convenience (and safety!) of our guests, we have organized transportation for guests staying
+	at the hotel to and from the venue! Currently, we are working on finalizing the scheduling
+	details, but you can plan on having transportation from the hotel to our venue before the ceremony
+	and back to the hotel at the conclusion of the night.
+</p>
+<p>
+	Final details, including transportation departure time, will be posted to this page when it is
+	available.
+</p>
