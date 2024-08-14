0411603adcfdab43ab2a90e3e3875dfd26e83ee5
Ash Johns <mail@ashpjohns.com>
2024-08-13 20:02:07 -0400

    add mroe guests

--- src/routes/rsvp/guestData.ts
+++ src/routes/rsvp/guestData.ts
@@ -1,448 +1,134 @@
 interface GuestCollection {
-  [key: string]: string[];
+	[key: string]: string[];
 }
 
 const guestData: GuestCollection = {
-  "ashsmells": [
-    "Cindy",
-    "Thomas"
-  ],
-  "caligurl": [
-    "Ivy Mwangi"
-  ],
-  "winnie": [
-    "Shannon Labelle",
-    "Dan Labelle"
-  ],
-  "kygo": [
-    "Songsong Le",
-    "Todd Thompson"
-  ],
-  "muppy": [
-    "Michelle Le",
-    "Mathew Lok"
-  ],
-  "millie": [
-    "Amanda Le",
-    "Matty Dubois",
-    "Mila"
-  ],
-  "12345678905409387261543245675hi800854983726536478909483": [
-    "Brandon Yannoni"
-  ],
-  "adamsmells": [
-    "Brandon Kania"
-  ],
-  "brandonsmells": [
-    "Adam Kania"
-  ],
-  "brandonflowers": [
-    "Ashley Johns",
-    "Juliette Johns"
-  ],
-  "ballislife": [
-    "Nayana Thimmiah"
-  ],
-  "welcomehome": [
-    "Mike Urbanowski",
-    "Eva Shrestha"
-  ],
-  "cooper": [
-    "Yen Anh",
-    "Ethan"
-  ],
-  "cara": [
-    "Cara"
-  ],
-  "doshi": [
-    "Mitali Doshi"
-  ],
-  "squidsquad": [
-    "Lauren Nguyen"
-  ],
-  "wooislife": [
-    "Katherine Sadianantz",
-    "Ethan"
-  ],
-  "tictac": [
-    "Ariana",
-    "Max"
-  ],
-  "surgeonhands": [
-    "Austin Howell",
-    "Elloria"
-  ],
-  "friendshiptrigangle": [
-    "Lisa Chan"
-  ],
-  "bame": [
-    "Mom",
-    "Dad"
-  ],
-  "le1": [
-    "Co Hai"
-  ],
-  "le2": [
-    "Co 7",
-    "Om 7"
-  ],
-  "le3": [
-    "Anh May",
-    "Guest 2",
-    "Guest 3",
-    "Guest 4",
-    "Guest 5"
-  ],
-  "le4": [
-    "Co 5",
-    "Om 5",
-    "Lisa",
-    "Chau"
-  ],
-  "le5": [
-    "Bac Tan"
-  ],
-  "le6": [
-    "Thomas",
-    "Bac Hoa"
-  ],
-  "le7": [
-    "Bac Tu",
-    "Bac Tu"
-  ],
-  "le8": [
-    "Anh Liem",
-    "Guest 2",
-    "Guest 3",
-    "Guest 4"
-  ],
-  "le9": [
-    "Han",
-    "Thang",
-    "Linh"
-  ],
-  "le10": [
-    "Tu",
-    "Mike",
-    "Kylie"
-  ],
-  "le11": [
-    "Van",
-    "Miggy",
-    "Talis"
-  ],
-  "bay": [
-    "Ong 7"
-  ],
-  "chi": [
-    "Ba Chi"
-  ],
-  "ongson": [
-    "Ong Son",
-    "Ba Huong"
-  ],
-  "hanh": [
-    "Ba Hanh",
-    "Ong Dung"
-  ],
-  "binh1": [
-    "Ba Binh"
-  ],
-  "binh2": [
-    "Chuong",
-    "Trang"
-  ],
-  "binh3": [
-    "Thoa",
-    "Phuoc"
-  ],
-  "binh4": [
-    "Khanh",
-    "Phuong"
-  ],
-  "kim1": [
-    "Bac Kim",
-    "Bac Thuan"
-  ],
-  "thuy": [
-    "Chi Thuy",
-    "Anh Sung",
-    "David",
-    "Tina"
-  ],
-  "thy": [
-    "Anh Thy",
-    "Anh Tai"
-  ],
-  "banhbao": [
-    "Chi Bu",
-    "Mario",
-    "Banh Bao"
-  ],
-  "lien": [
-    "Bac Lien"
-  ],
-  "chau": [
-    "Bac Chau"
-  ],
-  "dong": [
-    "Bac Dong",
-    "Bac Chi"
-  ],
-  "anhmai": [
-    "Chi Dao",
-    "Anh Mai"
-  ],
-  "clamchowder": [
-    "Anh Dat",
-    "Hoi Yan",
-    "Aurile",
-    "Alec"
-  ],
-  "number5": [
-    "Bac Xuan",
-    "Bac Oanh",
-    "Huy"
-  ],
-  "vinh": [
-    "Vinh",
-    "Vinh +1",
-  ],
-  "kimanh": [
-    "Kim Anh",
-    "Guest 2",
-    "Guest 3",
-    "Guest 4",
-    "Guest 5"
-  ],
-  "tran1": [
-    "Bac Tam",
-    "Bac Hong",
-    "Thong",
-    "Thong +1"
-  ],
-  "tran2": [
-    "Anh Co",
-    "Anh Co partner",
-    "Child 1",
-    "Child 2",
-    "Child 3"
-  ],
-  "tran3": [
-    "Minh Nhi",
-    "Patrick"
-  ],
-  "pochita": [
-    "Di Mai",
-    "Chu Phuc",
-    "Phi",
-    "Dan"
-  ],
-  "bui1": [
-    "Di Tien"
-  ],
-  "bui2": [
-    "Kimmi",
-    "Daniel"
-  ],
-  "huong": [
-    "Ba Huong"
-  ],
-  "kieu": [
-    "Ma Kieu",
-    "Bac Loc"
-  ],
-  "lekania1": [
-    "Ba Thuy"
-  ],
-  "lekania2": [
-    "Di Ky",
-    "Chu Thich"
-  ],
-  "lekania3": [
-    "Di Linh",
-    "Chu Liem"
-  ],
-  "lekania4": [
-    "Di Tho",
-    "Chu Thuan"
-  ],
-  "lekania5": [
-    "Chuong",
-    "Linh"
-  ],
-  "lekania6": [
-    "Di Tu",
-    "Guest 2"
-  ],
-  "lekania7": [
-    "Bac Phung"
-  ],
-  "lok": [
-    "Waion",
-    "Inmui"
-  ],
-  "nh1": [
-    "Bac Cau",
-    "Bac Cau"
-  ],
-  "nh2": [
-    "Van",
-    "Tho"
-  ],
-  "nh3": [
-    "Thy",
-    "Thuan"
-  ],
-  "nh4": [
-    "Vi",
-    "Tuan"
-  ],
-  "nh5": [
-    "Lap",
-    "Hoang"
-  ],
-  "nh6": [
-    "Duy",
-    "Phuong"
-  ],
-  "bailey1012": [
-    "Lynn Kania",
-    "Joseph Kania"
-  ],
-  "poppop": [
-    "Pop Pop"
-  ],
-  "golfislife": [
-    "Jim Fenner",
-    "Allisen Tencza"
-  ],
-  "beepjeep": [
-    "Paul Fenner",
-    "Allyson Campagna",
-    "Ashley Foss",
-    "Axel Foss"
-  ],
-  "buddy": [
-    "Buddy Fenner"
-  ],
-  "favoriteuncle3": [
-    "Steven Fenner",
-    "Renee Fenner",
-    "Holly Fenner"
-  ],
-  "paxton": [
-    "Daniel Fenner",
-    "Jennifer Fenner",
-    "Mackenzie Fenner",
-    "Andrew Fenner"
-  ],
-  "fenner2": [
-    "Chuck Fenner",
-    "Mary Fenner"
-  ],
-  "fenner1": [
-    "Alex Fenner",
-    "Ana Fenner"
-  ],
-  "favoriteuncle2": [
-    "John Kania"
-  ],
-  "golombki": [
-    "Mary Juges",
-    "Kari Juges",
-    "Becky Juges",
-    "Elizabeth Dalfonso",
-    "John Moorby",
-    "Mark Greenwich"
-  ],
-  "usa": [
-    "Gertrude Roembke",
-    "Uwe Roembke",
-    "Martin Roembke",
-    "Maria Roembke"
-  ],
-  "germany": [
-    "Albert Steffens",
-    "Deborah Steffens",
-    "Albert Steffens",
-    "Otto Steffens"
-  ],
-  "cheesecake": [
-    "Julie Eaton",
-    "John Eaton",
-    "Samantha Eaton",
-    "Lindsey Eaton"
-  ],
-  "soccer": [
-    "Jean Paul Galicia",
-    "Germania Galicia Rolas"
-  ],
-  "mwangi": [
-    "Charles Mwangi",
-    "Sarah Mwangi"
-  ],
-  "lekania8": [
-    "Agnieska Lozada",
-    "Jack Lozada",
-    "Mikayla Lozada",
-    "Emily Lozada"
-  ],
-  "lekania9": [
-    "Krystyna Piascik"
-  ],
-  "lekania10": [
-    "Marysia Malkowski"
-  ],
-  "lekania11": [
-    "Andy Rogalski",
-    "Sandy Rogalski"
-  ],
-  "lekania12": [
-    "Erik Rogalski",
-    "Jennifer Rogalski"
-  ],
-  "lekania13": [
-    "Alyssa Randle",
-    "Jesse Randle"
-  ],
-  "lekania14": [
-    "Stan Rogalski",
-    "Maryjane Rogalski"
-  ],
-  "lekania15": [
-    "Joseph Rogalski",
-    "Liz Rogalski"
-  ],
-  "lekania16": [
-    "Michele Rogalski-Kearsley",
-    "Brian Kearsley"
-  ],
-  "lekania17": [
-    "Krzysztof Kania",
-    "Patti Kania",
-    "Jessica Kania"
-  ],
-  "lekania18": [
-    "Zenon Malkowski",
-    "Biata Malkowski"
-  ],
-  "lekania19": [
-    "Magdalena Johnson",
-    "Adam Johnson"
-  ],
-  "lekania20": [
-    "Leszek Piascik",
-    "Bozena Piascik"
-  ],
-  "lekania21": [
-    "Slawek Piascik",
-    "Janina Piascik"
-  ],
-  "lekania22": [
-    "Kamil Piascik",
-    "Jessica Piascik"
-  ]
+	ashsmells: ['Cindy', 'Thomas'],
+	caligurl: ['Ivy Mwangi'],
+	winnie: ['Shannon Labelle', 'Dan Labelle'],
+	kygo: ['Songsong Le', 'Todd Thompson'],
+	muppy: ['Michelle Le', 'Mathew Lok'],
+	millie: ['Amanda Le', 'Matty Dubois', 'Mila'],
+	'12345678905409387261543245675hi800854983726536478909483': ['Brandon Yannoni'],
+	adamsmells: ['Brandon Kania'],
+	brandonsmells: ['Adam Kania'],
+	brandonflowers: ['Ashley Johns', 'Juliette Johns'],
+	ballislife: ['Nayana Thimmiah'],
+	welcomehome: ['Mike Urbanowski', 'Eva Shrestha'],
+	cooper: ['Yen Anh', 'Ethan'],
+	cara: ['Cara'],
+	doshi: ['Mitali Doshi'],
+	squidsquad: ['Lauren Nguyen'],
+	wooislife: ['Katherine Sadianantz', 'Ethan'],
+	tictac: ['Ariana', 'Max'],
+	surgeonhands: ['Austin Howell', 'Elloria'],
+	friendshiptrigangle: ['Lisa Chan'],
+	bame: ['Mom', 'Dad'],
+	le1: ['Co Hai'],
+	le2: ['Co 7', 'Om 7'],
+	le3: ['Anh May', 'Guest 2', 'Guest 3', 'Guest 4', 'Guest 5'],
+	le4: ['Co 5', 'Om 5', 'Lisa', 'Chau'],
+	le5: ['Bac Tan'],
+	le6: ['Thomas', 'Bac Hoa'],
+	le7: ['Bac Tu', 'Bac Tu'],
+	le8: ['Anh Liem', 'Guest 2', 'Guest 3', 'Guest 4'],
+	le9: ['Han', 'Thang', 'Linh'],
+	le10: ['Tu', 'Mike', 'Kylie'],
+	le11: ['Van', 'Miggy', 'Talis'],
+	bay: ['Ong 7'],
+	chi: ['Ba Chi'],
+	ongson: ['Ong Son', 'Ba Huong'],
+	hanh: ['Ba Hanh', 'Ong Dung'],
+	binh1: ['Ba Binh'],
+	binh2: ['Chuong', 'Trang'],
+	binh3: ['Thoa', 'Phuoc'],
+	binh4: ['Khanh', 'Phuong'],
+	kim1: ['Bac Kim', 'Bac Thuan'],
+	thuy: ['Chi Thuy', 'Anh Sung', 'David', 'Tina'],
+	thy: ['Anh Thy', 'Anh Tai'],
+	banhbao: ['Chi Bu', 'Mario', 'Banh Bao'],
+	lien: ['Bac Lien'],
+	chau: ['Bac Chau'],
+	dong: ['Bac Dong', 'Bac Chi'],
+	anhmai: ['Chi Dao', 'Anh Mai'],
+	clamchowder: ['Anh Dat', 'Hoi Yan', 'Aurile', 'Alec'],
+	number5: ['Bac Xuan', 'Bac Oanh', 'Huy'],
+	vinh: ['Vinh', 'Vinh +1'],
+	kimanh: ['Kim Anh', 'Guest 2', 'Guest 3', 'Guest 4', 'Guest 5'],
+	tran1: ['Bac Tam', 'Bac Hong', 'Thong', 'Thong +1'],
+	tran2: ['Anh Co', 'Anh Co partner', 'Child 1', 'Child 2', 'Child 3'],
+	tran3: ['Minh Nhi', 'Patrick'],
+	pochita: ['Di Mai', 'Chu Phuc', 'Phi', 'Dan'],
+	bui1: ['Di Tien'],
+	bui2: ['Kimmi', 'Daniel'],
+	huong: ['Ba Huong'],
+	kieu: ['Ma Kieu', 'Bac Loc'],
+	lekania1: ['Ba Thuy'],
+	lekania2: ['Di Ky', 'Chu Thich'],
+	lekania3: ['Di Linh', 'Chu Liem'],
+	lekania4: ['Di Tho', 'Chu Thuan'],
+	lekania5: ['Chuong', 'Linh'],
+	lekania6: ['Di Tu', 'Guest 2'],
+	lekania7: ['Bac Phung'],
+	lok: ['Waion', 'Inmui'],
+	nh1: ['Bac Cau', 'Bac Cau'],
+	nh2: ['Van', 'Tho'],
+	nh3: ['Thy', 'Thuan'],
+	nh4: ['Vi', 'Tuan'],
+	nh5: ['Lap', 'Hoang'],
+	nh6: ['Duy', 'Phuong'],
+	bailey1012: ['Lynn Kania', 'Joseph Kania'],
+	poppop: ['Pop Pop'],
+	golfislife: ['Jim Fenner', 'Allisen Tencza'],
+	beepjeep: ['Paul Fenner', 'Allyson Campagna', 'Ashley Foss', 'Axel Foss'],
+	buddy: ['Buddy Fenner'],
+	favoriteuncle3: ['Steven Fenner', 'Renee Fenner', 'Holly Fenner'],
+	paxton: ['Daniel Fenner', 'Jennifer Fenner', 'Mackenzie Fenner', 'Andrew Fenner'],
+	fenner2: ['Chuck Fenner', 'Mary Fenner'],
+	fenner1: ['Alex Fenner', 'Ana Fenner'],
+	favoriteuncle2: ['John Kania'],
+	golombki: [
+		'Mary Juges',
+		'Kari Juges',
+		'Becky Juges',
+		'Elizabeth Dalfonso',
+		'John Moorby',
+		'Mark Greenwich'
+	],
+	usa: ['Gertrude Roembke', 'Uwe Roembke', 'Martin Roembke', 'Maria Roembke'],
+	germany: ['Albert Steffens', 'Deborah Steffens', 'Albert Steffens', 'Otto Steffens'],
+	cheesecake: ['Julie Eaton', 'John Eaton', 'Samantha Eaton', 'Lindsey Eaton'],
+	soccer: ['Jean Paul Galicia', 'Germania Galicia Rolas'],
+	mwangi: ['Charles Mwangi', 'Sarah Mwangi'],
+	lekania8: ['Agnieska Lozada', 'Jack Lozada', 'Mikayla Lozada', 'Emily Lozada'],
+	lekania9: ['Krystyna Piascik'],
+	lekania10: ['Marysia Malkowski'],
+	lekania11: ['Andy Rogalski', 'Sandy Rogalski'],
+	lekania12: ['Erik Rogalski', 'Jennifer Rogalski'],
+	lekania13: ['Alyssa Randle', 'Jesse Randle'],
+	lekania14: ['Stan Rogalski', 'Maryjane Rogalski'],
+	lekania15: ['Joseph Rogalski', 'Liz Rogalski'],
+	lekania16: ['Michele Rogalski-Kearsley', 'Brian Kearsley'],
+	lekania17: ['Krzysztof Kania', 'Patti Kania', 'Jessica Kania'],
+	lekania18: ['Zenon Malkowski', 'Biata Malkowski'],
+	lekania19: ['Magdalena Johnson', 'Adam Johnson'],
+	lekania20: ['Leszek Piascik', 'Bozena Piascik'],
+	lekania21: ['Slawek Piascik', 'Janina Piascik'],
+	lekania22: ['Kamil Piascik', 'Jessica Piascik'],
+	sangriapals: ['George Tarabelsi'],
+	ohcanada: ['Jeneva Smith'],
+	drdom: ['Dominique Kaempf'],
+	umass4life: ['Sara Hyde'],
+	welovevermont: ['Noah Manning'],
+	lola: ['Catrina Goulet'],
+	docmartin: ['Irene Martin'],
+	drtrebilcock: ['Jennifer Trebilcock'],
+	drfabulous: ['Molly Gallagher'],
+	drwriter: ['Sarah Wu'],
+	queen: ['Kathryn Addonizio'],
+	code: ['Kavya Sudireddy'],
+	internoftheyear: ['Michelle Nabi'],
+	bbq: ['Venus Huang']
 };
 
 export default guestData;
